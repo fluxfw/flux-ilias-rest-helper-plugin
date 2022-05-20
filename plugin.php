6a611ba1751831d021f6c702ec1305be581750c7
fluxfw <fluxfw@fluxlabs.ch>
2022-05-20 09:09:23 +0200

    disable ILIAS 8

--- plugin.php
+++ plugin.php
@@ -3,6 +3,6 @@
 $id = "flilrehepl";
 $version = "1.0.0";
 $ilias_min_version = "6.0";
-$ilias_max_version = "8.999";
+$ilias_max_version = "7.999";
 $responsible = "fluxlabs";
 $responsible_mail = "support@fluxlabs.ch";
