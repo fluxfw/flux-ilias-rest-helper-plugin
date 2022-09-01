c9349e2686f746a25fc305dc8d6598840b9c3d45
fluxfw <fluxfw@fluxlabs.ch>
2022-09-01 12:19:24 +0200

    ILIAS 8 workaround for SuperGlobalDropInReplacement

--- classes/class.ilflux_ilias_rest_helper_pluginUIHookGUI.php
+++ classes/class.ilflux_ilias_rest_helper_pluginUIHookGUI.php
@@ -1,13 +1,18 @@
 <?php
 
+use ILIAS\HTTP\Wrapper\SuperGlobalDropInReplacement;
+use ILIAS\Refinery\KeyValueAccess;
+
 /**
  * @property ilflux_ilias_rest_helper_pluginPlugin $plugin_object
  */
 class ilflux_ilias_rest_helper_pluginUIHookGUI extends ilUIHookPluginGUI
 {
 
-    public function getHTML(/*string*/ $a_comp, /*string*/ $a_part, $a_par = []) : array
+    public function getHTML(/*string*/ $a_comp, /*string*/ $a_part, /*array*/ $a_par = []) : array
     {
+        $this->fixSuperGlobalDropInReplacements();
+
         if ($a_comp === "Services/Utilities" && $a_part === "redirect") {
             $url = $this->plugin_object::getIliasApi()
                 ->handleIliasRedirect(
@@ -27,7 +32,37 @@ class ilflux_ilias_rest_helper_pluginUIHookGUI extends ilUIHookPluginGUI
 
     public function gotoHook() : void
     {
+        $this->fixSuperGlobalDropInReplacements();
+
         $this->plugin_object::getIliasApi()
             ->handleIliasGoto();
+    }
+
+
+    private function fixSuperGlobalDropInReplacements() : void {
+        if (!class_exists(SuperGlobalDropInReplacement::class)) {
+            return;
+        }
+
+        $this->fixSuperGlobalDropInReplacement(
+            "_GET"
+        );
+        $this->fixSuperGlobalDropInReplacement(
+            "_POST"
+        );
+        $this->fixSuperGlobalDropInReplacement(
+            "_COOKIE"
+        );
+    }
+
+
+    private function fixSuperGlobalDropInReplacement(string $name) : void {
+        if (is_array($GLOBALS[$name]) || !($GLOBALS[$name] instanceof SuperGlobalDropInReplacement)) {
+            return;
+        }
+
+        $GLOBALS[$name] = Closure::bind(function() : array {
+            return $this->raw_values;
+        }, $GLOBALS[$name], KeyValueAccess::class)();
     }
 }
